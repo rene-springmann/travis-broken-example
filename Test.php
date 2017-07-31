66c543f78175a9fdd2c96919df127ea827c7c45d
renesp <r-springmann@sokrates.de>
2017-07-31 12:39:33 +0200

    license

--- Test.php
+++ Test.php
@@ -1,6 +1,8 @@
 <?php
 
-class Test extends PHPUnit_Framework_TestCase
+class_alias('\PHPUnit_Framework_TestCase', 'PHPUnit\Framework\TestCase');
+
+class Test extends PHPUnit\Framework\TestCase
 {
 	public function testOnePlusOne() {
 		$this->assertEquals(1+1, 2);
